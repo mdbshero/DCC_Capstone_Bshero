8d9224e7bd95f8d85a2247d6744597a6b39f7067
Mdbshero <mdbshero@gmail.com>
2022-05-17 21:09:34 -0500

    added styling to verification page for agencies.

--- frontend/src/pages/VerPage/VerPageAgency.js
+++ frontend/src/pages/VerPage/VerPageAgency.js
@@ -83,68 +83,141 @@ const VerPageAgency = () => {
   return (
     <div>
       <NavbarAgency />
-      <div>
-        <h3>Friends Page for {user.name}!</h3>
-        <div>
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  <th>Verfied Users</th>
-                </tr>
-              </thead>
-              <tbody>
-                {ver &&
-                  ver.map((v, index) => {
-                    return (
-                      <tr key={index}>
-                        <td>
-                          <img src={`http://localhost:3011/${v.image}`}></img>
-                          <h5>{v.name}</h5>
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
+      <div className="container-fluid">
+        <div className="text-center">
+          <h3>Verification Page for {user.name}!</h3>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="border border-3">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Verfied Users</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {ver &&
+                    ver.map((v, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>
+                            <img src={`http://localhost:3011/${v.image}`}></img>
+                            <h4>{v.name}</h4>
+                            <h6>{v.contact.street}</h6>
+                            <h6>{v.contact.city}</h6>
+                            <h6>{v.contact.state}</h6>
+                            <h6>{v.contact.zip}</h6>
+                            <h6>{v.contact.phone}</h6>
+                            <h6>{v.email}</h6>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  <th> Pending User Requests </th>
-                </tr>
-              </thead>
-              <tbody>
-                {pending &&
-                  pending.map((pend, index) => {
-                    return (
-                      <tr key={index}>
-                        <td>
-                          <img
-                            src={`http://localhost:3011/${pend.image}`}
-                          ></img>
-                          <h5>{pend.name}</h5>
-                          <button
-                            type="accept"
-                            id="acceptPendingButton"
-                            onClick={(event) => handleClickAccept(event, pend)}
-                          >
-                            Accept
-                          </button>
-                          <button
-                            type="delete"
-                            id="declinePendingButton"
-                            onClick={(event) => handleClickDecline(event, pend)}
-                          >
-                            Decline
-                          </button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
+          <div className="col-md-6">
+            <div className="border border-3">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th> Pending User Requests </th>
+                    <th>Verification Information</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pending &&
+                    pending.map((pend, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>
+                            <img
+                              src={`http://localhost:3011/${pend.image}`}
+                            ></img>
+                            <h5>{pend.name}</h5>
+                            <h6>{pend.contact.phone}</h6>
+                            <h6>{pend.email}</h6>
+                            <button
+                              type="accept"
+                              className="btn btn-outline-dark btn-sm px-3"
+                              id="acceptPendingButton"
+                              onClick={(event) =>
+                                handleClickAccept(event, pend)
+                              }
+                            >
+                              Accept
+                            </button>
+                            <button
+                              type="delete"
+                              className="btn btn-outline-dark btn-sm px-3"
+                              id="declinePendingButton"
+                              onClick={(event) =>
+                                handleClickDecline(event, pend)
+                              }
+                            >
+                              Decline
+                            </button>
+                          </td>
+                          <td>
+                            Employment: {pend.verification.employment}
+                            <br />
+                            Type of Home: {pend.verification.homeType}
+                            <br />
+                            Housing Status: {pend.verification.homeStatus}
+                            <br />
+                            Time in Home: {pend.verification.homeTime}
+                            <br />
+                            Home Noise Level: {pend.verification.homeNoise}
+                            <br />
+                            Landlord Name: {pend.verification.landName}
+                            <br />
+                            Landlord Number: {pend.verification.landNumber}
+                            <br />
+                            Number of Adults: {pend.verification.aNum}
+                            <br />
+                            Number of Children: {pend.verification.cNum}
+                            <br />
+                            Reason for Adoption: {pend.verification.adoptReason}
+                            <br />
+                            Average time pet will be alone per day:{" "}
+                            {pend.verification.petHours}
+                            <br />
+                            EmployWhere pet will be kept during dayment:{" "}
+                            {pend.verification.petLoca}
+                            <br />
+                            Where pet will sleep: {pend.verification.petSleep}
+                            <br />
+                            Fencing: {pend.verification.fence}
+                            <br />
+                            Veterinarian Name: {pend.verification.vetName}
+                            <br />
+                            Veterinarian Number: {pend.verification.vetNumber}
+                            <br />
+                            Preferred Temperment: {pend.verification.prefTemp}
+                            <br />
+                            Dislikes: {pend.verification.petDis}
+                            <br />
+                            Pet Energy Level: {pend.verification.petEn}
+                            <br />
+                            Ideals: {pend.verification.petIdeal}
+                            <br />
+                            Untolerable Habits Habitsoyment:{" "}
+                            {pend.verification.petBadHab}
+                            <br />
+                            Agreement One: {pend.verification.agreeOne}
+                            <br />
+                            Agreement Two: {pend.verification.agreeTwo}
+                            <br />
+                            Agreement Three: {pend.verification.agreeThree}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
